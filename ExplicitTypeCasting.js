add2fbbc244d54346162c13d5e5cb68a0636b608
Bharathraj526 <151908705+Bharathraj526@users.noreply.github.com>
2025-10-10 21:33:05 +0530

    Add files via upload
    Typecasting in javaScript

---
+++ ExplicitTypeCasting.js
@@ -0,0 +1,7 @@
+let two="2";
+
+
+
+
+let numberOne="10lpa";
+console.log(parseInt(numberOne),typeof parseInt(numberOne));
---
+++ ImplicitDataTypeCasting.js
@@ -0,0 +1,7 @@
+let numberOne="20";
+let numberTwo=2;
+console.log(numberOne,typeof(numberOne));//20 string
+console.log(numberOne+numberTwo,typeof(numberOne+numberTwo));//202 string(no implicit convertion)
+console.log(numberOne/numberTwo, typeof(numberOne/numberTwo));//10 'number' (implicit convertion possible)
+console.log(numberOne*numberTwo, typeof(numberOne*numberTwo));//40 'number' (implicit convertion possible)
+console.log(numberOne-numberTwo, typeof(numberOne-numberTwo));//18 'number' (implicit convertion possible)
