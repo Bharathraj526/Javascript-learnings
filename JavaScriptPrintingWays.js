1dc8eee27cc0c6c0286b138194debc87a1ebf1e6
Bharathraj526 <151908705+Bharathraj526@users.noreply.github.com>
2025-10-11 21:01:50 +0530

    Add files via upload

---
+++ JavaScriptPrintingWays.js
@@ -0,0 +1,26 @@
+//java script printing ways
+//console object(browserConsole)
+console.log("javaScriptPrintingWays");
+console.log(10+5);//15
+console.log(10*5);//50
+console.log(console);//console object ,normal message form
+console.dir(console);//console object,object form
+console.dir(document);//document object,obj form
+console.dir(window);//window obj ,obj form
+
+
+//document object
+document.write("<span style='color:red;background-color:white'>FirstLine</span><br>");
+document.write("<span style='color:white;background-color:red'>SecondLine</span><br>");
+document.writeln("<span style='color:blue;background-color:white'>ThirdLine</span><br>");
+document.writeln("<span style='color:red;background-color:black'>FourthLine</span><br>");
+document.writeln("<span style='color:pink;background-color:green'>FifthLine</span><br>");
+
+//window object
+
+//innerHTML java script property
+//dom
+document.getElementById("container_one").innerHTML="<div style='width:50%;height:50%;background-color:yellow;'><div style='width:50%;height:50%;background-color:darkblue;'></div></div>"
+//innerTExt java script property
+//dom
+document.getElementById("container_two").innerText="<div style='width:50%;height:50%;background-color:yellow;'><div style='width:50%;height:50%;background-color:darkblue;'></div></div>";
