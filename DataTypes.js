34ec7c4ecad1f6466a56a4891e210be643a4e428
Bharathraj526 <151908705+Bharathraj526@users.noreply.github.com>
2025-10-12 07:01:30 +0530

    Add files via upload

---
+++ DataTypes.js
@@ -0,0 +1,53 @@
+// String
+let color = "Yellow";
+let lastName = "Johnson";
+console.log(color,typeof lastName);
+console.log(lastName,typeof lastName)
+// Number
+let length = 16;
+let weight = 7.5;
+console.log(length,typeof length);
+console.log(weight,typeof weight);
+
+// BigInt
+let x = 1234567890123456789012345n;
+let y = BigInt(1234567890123456789012345n);
+console.log(x ,typeof x);
+console.log(y ,typeof y);
+
+
+// Boolean
+let a = true;
+let b = false;
+console.log(a,typeof a);
+console.log(b,typeof b);
+
+// Object
+const person = {firstName:"John", lastName:"Doe"};
+console.log(person,typeof person);
+
+// Array object
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars,typeof cars);
+
+// Date object
+const date = new Date("2022-03-25");
+console.log(date,typeof date);
+
+// Undefined
+let c;
+let d;
+console.log(c,typeof c);
+console.log(d,typeof d);
+
+// Null
+let e= null;
+let f = null;
+console.log(e,typeof e);
+console.log(f,typeof f);
+
+// Symbol
+const g = Symbol();
+const h = Symbol();
+console.log(g,typeof g);
+console.log(h,typeof h);
